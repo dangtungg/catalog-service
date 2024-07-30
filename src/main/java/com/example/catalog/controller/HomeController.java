c4c4a3ce4a6197c2ca028833fb802d8a5b495116
Tung Dang <dtt.dangthanhtung@gmail.com>
2024-07-30 15:36:46 +0700

    Example of how to use properties

--- src/main/java/com/example/catalog/controller/HomeController.java
+++ src/main/java/com/example/catalog/controller/HomeController.java
@@ -1,14 +1,21 @@
 package com.example.catalog.controller;
 
+import com.example.catalog.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
+
     @GetMapping("/")
     public String getGreetings() {
-        return "Welcome to the catalog service!";
+        return polarProperties.getGreeting();
     }
 
 }
